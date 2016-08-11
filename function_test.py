e4177aacf70c3ce4c277ec0e75a6275bbc888a70
pellok <pellok@okborn.com>
2016-08-11 13:47:44 +0800

    重構使用Template

--- function_test.py
+++ function_test.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -25,6 +26,21 @@ class NewVisitorTest(unittest.TestCase):
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
         )
+
+        # 他在文字方塊輸入"購買孔雀羽毛"
+        # (Edith的興趣事綁蒼蠅魚餌)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 當他按下Enter時，網頁會更新，現在網頁列出
+        # "1：購買孔雀羽毛" 一個待辦事項清單項目
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows)
+            )
+
         self.fail('Finish the test')
 
 if __name__ == '__main__':
