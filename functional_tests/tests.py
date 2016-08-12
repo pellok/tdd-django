243c9c850c9c2d84646c2ed2e873b8f8ff2fe561
pellok <pellok@okborn.com>
2016-08-12 17:59:42 +0800

    讓functional test 變成app 使用LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
-import unittest
+# encoding=utf8
+from __future__ import unicode_literals
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 他去查看首頁
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # 發現網頁標題與標頭顯示代辦事項清單
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -41,20 +43,16 @@ class NewVisitorTest(unittest.TestCase):
         # "1：購買孔雀羽毛" 一個待辦事項清單項目
         inputbox.send_keys(Keys.ENTER)
 
-        #讓他可以加入另外一個項目
-        #她輸入"使用孔雀羽毛來製作一隻蒼蠅"
+        # 讓他可以加入另外一個項目
+        # 她輸入"使用孔雀羽毛來製作一隻蒼蠅"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-
-        #在次更新網頁，現在他的清單有這兩個項目
+        # 在次更新網頁，現在他的清單有這兩個項目
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # import time
         # time.sleep(10)
         self.fail('Finish the test')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
