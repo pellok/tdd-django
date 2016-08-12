5d2983ef50f8e4d423833941ba14de2a02429306
pellok <pellok@okborn.com>
2016-08-12 12:15:36 +0800

    第一個model 和 單元測試

--- lists/tests.py
+++ lists/tests.py
@@ -7,7 +7,10 @@ from lists.views import home_page
 from django.http import HttpRequest
 from lists.views import home_page
 
+from lists.models import Item
+
 from django.template.loader import render_to_string
+
 
 class HomePageTest(TestCase):
 
@@ -33,3 +36,27 @@ class HomePageTest(TestCase):
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expceted_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saveing_and_retrieving_item(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+
