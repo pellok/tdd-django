440f817028d7b03dded55f6b0ae440a6a98eec2a
pellok <pellok@okborn.com>
2016-08-12 11:50:53 +0800

    重構function_text

--- function_test.py
+++ function_test.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 他去查看首頁
         self.browser.get('http://localhost:8000')
@@ -43,11 +49,8 @@ class NewVisitorTest(unittest.TestCase):
 
 
         #在次更新網頁，現在他的清單有這兩個項目
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # import time
         # time.sleep(10)
